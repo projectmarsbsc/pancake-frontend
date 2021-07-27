c8f380221073a32cf6499330ec31bffe20d578f9
Project Mars <88032643+projectmarsbsc@users.noreply.github.com>
2021-07-27 12:45:11 +0100

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,20 +7,20 @@ const farms: FarmConfig[] = [
    */
   {
     pid: 0,
-    lpSymbol: 'CAKE',
+    lpSymbol: 'FUEL',
     lpAddresses: {
-      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      97: '',
+      56: '0x4035652516911d7784Ed3C79462657130d86Aea9',
     },
     token: tokens.syrup,
     quoteToken: tokens.wbnb,
   },
   {
     pid: 251,
-    lpSymbol: 'CAKE-BNB LP',
+    lpSymbol: 'FUEL-BNB LP',
     lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+      97: '',
+      56: '0x4035652516911d7784Ed3C79462657130d86Aea9',
     },
     token: tokens.cake,
     quoteToken: tokens.wbnb,
